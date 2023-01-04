7513a061648c326b43cefa7389e2a0f8c3246ce0
maryem <chaabamaryem@gmail.com>
2023-01-04 13:54:39 +0100

    create header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+
       <NavBar/>
-    </div>
+      <Header/>
+    </>
   );
 }
 
